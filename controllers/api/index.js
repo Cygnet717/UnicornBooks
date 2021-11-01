224ab87eff1ccb85cb7dbd071a89e6dadb788390
Dillon Cleaver <cleadi@fastmail.com>
2021-11-01 11:56:15 -0500

    Initial commit. Added some starter code to controllers folder.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,8 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const projectRoutes = require("./projectRoutes"); // placeholder
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes); // placeholder
 
 module.exports = router;
