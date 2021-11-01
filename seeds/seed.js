bec13ed3dc14134864c2d12fdebf06aaa8d832ce
Frost <mpyang2021@gmail.com>
2021-11-01 11:21:16 -0500

    added userData.json and seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Project } = require('../models');
+
+const userData = require('./userData.json');
+const projectData = require('./booktData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+ await Book.bulkCreate(bookData, {
+    individualHooks: true,
+    returning: true,
+ });
+
+  process.exit(0);
+};
+
+seedDatabase();
