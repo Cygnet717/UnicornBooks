a79eb7dabef3c10891812d6e43c5b3b9e95d7716
Cygnet717 <kathybradt717@gmail.com>
2021-11-02 13:19:54 -0500

    fixed route bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,36 @@ const router = require("express").Router();
 const { Book, Checkout, Location, User } = require("../models");
 const withAuth = require("../utils/auth");
 
+
+
+router.get("/settings", withAuth, async (req, res) => {
+  console.log('settings page')
+  try {
+    const userData = await User.findByPk(req.session.user_id);
+
+    const user = userData.get({ plain: true });
+
+    res.render("settings", {
+      ...user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  console.log('################this')
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
 router.get("/", async (req, res) => {
+  console.log('home page')
   try {
     const bookData = await Book.findAll();
 
@@ -36,30 +65,6 @@ router.get("/:id", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-});
-
-router.get("/settings", withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id);
-
-    const user = userData.get({ plain: true });
-
-    res.render("settings", {
-      ...user,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/login", (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/dashboard");
-    return;
-  }
-
-  res.render("login");
 });
 
 module.exports = router;
