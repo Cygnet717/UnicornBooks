26029cc0307da71a385882f9cbcd5eb76117554d
Cygnet717 <kathybradt717@gmail.com>
2021-11-03 10:16:53 -0500

    dropdownjs might work

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,15 +1,25 @@
-const singleOption = document.querySelector('.autobiog')
 const selectGenera = document.querySelector('#selectGenera')
+const selectLocation = document.querySelector('#selectLocation')
 
 
 function sortByGenera (event) {
-  console.log(event.target.value)
-  //const response = await fetch('/route....', {
-    //method: 'POST',
-    //body: JSON.stringify({genera: event.target.value})
-    //headers: { 'Content-Type': 'application/json' },
-  //})
-//sends chosen value to home route that pulls correct data and renders page
+  const genre = event.target.value
+  const response = await fetch(`/genre/${genre}`, {
+    method: 'POST',
+    body: JSON.stringify({genera: event.target.value}),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  
+}
+
+function sortByLocation (event) {
+  const location = event.target.value
+  const response = await fetch(`/location/${location}`, {
+    method: 'POST',
+    body: JSON.stringify({genera: event.target.value}),
+    headers: { 'Content-Type': 'application/json' },
+  })
 }
 
 selectGenera.addEventListener('change', sortByGenera)
+selectLocation.addEventListener('change', sortByLocation)
