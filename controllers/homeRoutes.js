445abc1280f55582cebe8df392746583ecf60c8d
Dillon Cleaver <cleadi@fastmail.com>
2021-11-03 10:40:11 -0500

    Still working on new routes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,53 @@ const router = require("express").Router();
 const { Book, Checkout, Location, User } = require("../models");
 const withAuth = require("../utils/auth");
 
+// checkbox route
+router.get("/:", (req, res) => {
+  const isCheckedOut = document.querySelectorAll("card-body");
+
+  isCheckedOut.forEach((book) => {
+    if (book.dataset.id === undefined) {
+      book.classList.remove("hide");
+    }
+  });
+});
+
+// location route
+router.get("location/:location", async (req, res) => {
+  try {
+    const locationData = await Location.findAll({
+      where: { location_name: req.params.location_name },
+    });
+
+    const locations = locationData.map((location) =>
+      location.get({ plain: true })
+    );
+
+    res.render("dashboard", {
+      ...locations,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/genre/:genre", async (req, res) => {
+  console.log("Test");
+  try {
+    const bookGenreData = await Book.findAll({
+      where: { genre: req.params.genre },
+    });
+
+    const books = bookGenreData.map((book) => book.get({ plain: true }));
+
+    res.render("dashboard", {
+      books,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
     const bookData = await Book.findAll();
