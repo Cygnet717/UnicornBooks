e7be4b90bedc2a667b11d2503c5d1e1136168fd4
Cygnet717 <kathybradt717@gmail.com>
2021-11-03 15:27:25 -0500

    fiddled with login

--- public/js/login.js
+++ public/js/login.js
@@ -26,14 +26,14 @@ const signupFormHandler = async (event) => {
   const username = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-  console.log(username);
+ 
   if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+console.log(response.ok)
     if (response.ok) {
       document.location.replace('/');
     } else {
