ba53c31c151b7a8ab9b8ea08667fc428523918b3
Cygnet717 <kathybradt717@gmail.com>
2021-11-04 10:57:17 -0500

    delete user complete

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,18 +1,17 @@
 
-const updateUserHandler = async (event) => {
-  // event.preventDefault();
+const updateUserHandler = async () => {
   const user_id = document.querySelector('#name-update').getAttribute('data-user_id');
   const username = document.querySelector('#name-update').value.trim();
   const email = document.querySelector('#email-update').value.trim();
  
   if (username && email) {
-    console.log('here')
+   
     const response = await fetch(`/api/users/${user_id}`, {
       method: 'PUT',
       body: JSON.stringify({ username, email }),
       headers: { 'Content-Type': 'application/json' },
     });
-console.log(response)
+
     if (response.ok) {
       document.location.replace(`/dashboard/${user_id}`);
     } else {
@@ -22,10 +21,19 @@ console.log(response)
 }
 
 
-const deleteUserHandler = async (event) => {
-  event.preventDefault();
-console.log(document.querySelector('#name-update').getAttribute('data-user_id'))
-  console.log('deleteUser')
+const deleteUserHandler = async () => {
+ const user_id = document.querySelector('#name-update').getAttribute('data-user_id')
+  
+ const response = await fetch(`/api/users/${user_id}`, {
+  method: 'DELETE',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+if (response.ok) {
+  document.location.replace(`/`);
+} else {
+  alert('Failed to update.');
+}
 }
 
 
