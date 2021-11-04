3b19d2c6a78da528c0cb1a7ac282def6c061d48b
Cygnet717 <kathybradt717@gmail.com>
2021-11-04 12:26:49 -0500

    added error notes and change page reload after checking out

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -2,7 +2,6 @@ const checkoutElement = document.querySelector('.checkoutButton').children
 const user_id = document.querySelector('.checkoutButton').getAttribute('data-user_id')
 
 async function checkoutBook(event){
-  console.log(event.target.getAttribute('data-user_id'))
   if(event.target.hasAttribute('data-book_id')){
     const book_id = event.target.getAttribute('data-book_id')
 
@@ -12,7 +11,8 @@ async function checkoutBook(event){
       headers: { 'Content-Type': 'application/json' },
     })
     if(response.ok){
-      document.location.replace(`/`)
+      console.log(document.location)
+      document.location.reload()
     }
   }
 }
